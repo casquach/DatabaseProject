2fa673049649bc2ea8fde661f42b7ab88f6c52ba
EC2 Default User <ec2-user@ip-10-0-0-226.ec2.internal>
2020-11-15 20:49:07 +0000

    unreadyphp

--- gamePage/index.php
+++ gamePage/index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
+<?php include "../../inc/dbinfo.inc"; ?>
 <head>
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 	<link rel="stylesheet" href="gamePage.css">
 	<link rel="stylesheet" href="" id="nav-css">
 </head>
 <body>
+	<?php
+		/* Connect to MySQL and select the database. */
+		$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+		if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		$database = mysqli_select_db($connection, DB_DATABASE);
+	?>
+
 	<div id="nav-placeholder">
 
 	</div>
@@ -37,13 +46,25 @@
 		<div class="column" style="margin:2.5%">
 			<h2 id ="gameName">WHAT</header>
 			<h3>Gamers: 69</h5>
-			<p>League of Legends is a free-to-play competitive MOBA game with a large following in Esports.</p>
+			<?php
+				$gameName = $_GET['myGame'];
+				$spacedName = ltrim(preg_replace('/(?<! )(?<!^)[A-Z0-9]/',' $0', $gameName));
+				$result = mysqli_query($connection, "SELECT * FROM game NATURAL JOIN hasGenres WHERE game_name=",$spacedName);
+				$query_data = mysqli_fetch_row($result);
+				echo "SELECT * FROM game NATURAL JOIN hasGenres WHERE game_name=".$spacedName;
+				echo $query_data[0];
+				echo $query_data[1];
+				echo $query_data[2];
+			?>
 		</div>
 	</div>
 	<div class="row">
 
 	</div>
 		
-
+	<?php
+		mysqli_free_result($result);
+		mysqli_close($connection);
+	?>
 </body>
 </html>
