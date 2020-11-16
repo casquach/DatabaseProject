4746b0d40323c58e416262e6a58a2ea62cbcf16d
EC2 Default User <ec2-user@ip-10-0-0-226.ec2.internal>
2020-11-16 00:57:50 +0000

    unfinished edit button

--- userPage/index.php
+++ userPage/index.php
@@ -37,16 +37,53 @@
   			echo '<img src="data:image/jpeg;base64,'.base64_encode( $query_data[0] ).'"/>';
 		?>
 		<div class="column" style="margin:2.5%">
-			<h2 id ="gameName">WHAT</h2>
 			<?php
 				$email = "cq5yc@virginia.edu";
+				$result = mysqli_query($connection, "SELECT firstName FROM users WHERE email= '" . $email . "'");
+                                $query_data = mysqli_fetch_row($result);
+                                echo "<h2>" . $query_data[0] . " ";
+
+				$result = mysqli_query($connection, "SELECT lastName FROM users WHERE email= '" . $email . "'");
+                                $query_data = mysqli_fetch_row($result);
+                                echo $query_data[0] . "</h2>";
+
 				$result = mysqli_query($connection, "SELECT username FROM users WHERE email= '" . $email . "'");
 				$query_data = mysqli_fetch_row($result);
 				echo "@" . $query_data[0];
 				echo "<p></p>";
+
 				$result = mysqli_query($connection, "SELECT bio FROM users WHERE email= '" . $email . "'");
 				$query_data = mysqli_fetch_row($result);
 				echo "Bio: " . $query_data[0];
+				echo "<p></p>";
+			?>
+			<form id="mailtext" method="post">
+                            <input type="submit" name="submit" value="Edit">
+                        </form>
+			<?php
+				if(isset($_POST['submit'])) {
+				   $query2 = "UPDATE mailtext SET text=? WHERE id=1";
+				   $stmt = mysqli_prepare($connection, $query2);
+				   mysqli_stmt_bind_param($stmt, "s", $text);
+				   mysqli_stmt_execute($stmt);
+				   echo 'Successfully saved!';
+				}
+
+				$query = "SELECT text FROM mailtext";
+				$result = mysqli_query($connection, $query);
+
+				while($row = mysqli_fetch_assoc($result))
+				{
+				    $text = iconv('iso-8859-2', 'utf-8', $row['text']);
+				    echo'
+					<center>
+						<form id="mailtext" method="post">
+    							<textarea name="text" style="width: 500px; height: 300px;">'.$text.'</textarea>
+        						<input type="submit" name="submit" value="Save">
+    						</form>
+					</center>
+					';
+				}
 			?>
 		</div>
 	</div>
